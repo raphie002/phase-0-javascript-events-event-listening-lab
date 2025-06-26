a0505e953f191b40bcc710881ab00bfbf6a86ab2
Raphael <raphael.njoroge02@gmail.com>
2025-06-26 21:33:11 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
+}
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+function addingEventListener() {
+  const input = document.getElementById('button');
+
+  input.addEventListener('click', clickAlert);
 }
